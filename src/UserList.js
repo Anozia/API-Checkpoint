3c413e303a1779ca3fa91a59c5d67da8ce9caa5f
Kelechi Anozia <kelechianozia@gmail.com>
2022-04-24 00:47:06 +0100

    Added Changes

--- src/UserList.js
+++ src/UserList.js
@@ -2,30 +2,82 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function UserList() {
-  const [posts, setPosts] = useState([]);
+    this.state = {
+        users: [],
+        isLoading: false,
+        isError: false
+      }
+    }
   
+async UserList() {
+    this.setState({ isLoading: true })
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    if (response.ok) {
+      const users = await response.json()
+      this.setState({ users, isLoading: false })
+    } else {
+      this.setState({ isError: true, isLoading: false })
+    }
+  }
   useEffect(() => {
     axios
-    .get(https://jsonplaceholder.typicode.com/users);
-    .then(response => {response.json()
-  .then(json => console.log(json))
-    }),
+.get(https://jsonplaceholder.typicode.com/users);
+    .then(res => {
+        return response.json()
+    })
+  .then(json => {
+    console.log(json)
+  }),[]);
     
 
   return(
 
-   <div>
-  <ul>
-      {
-user.map(user => <li key = {user.id}>{user.title}</li>,
-user.map(user => <li key = {user.name}>{user.title}</li>,
-user.map(user => <li key = {user.email}>{user.title}</li>,
-user.map(user => <li key = {user.street}>{user.title}</li>,
-user.map(user => <li key = {user.address}>{user.title}</li>,
-user.map(user => <li key = {user.phone}>{user.title}</li>)
-</ul>
-  
-  </div>;
-})
+    render() {
+        const { users, isLoading, isError } = this.state
+     
+        if (isLoading) {
+          return <div>Loading...</div>
+        }
+     
+        if (isError) {
+          return <div>Error</div>
+        }
+     
+        return users.length > 0
+          ? (
+            <table>
+              <thead>
+                <tr>
+                  {this.renderTableHeader()}
+                </tr>
+              </thead>
+              <tbody>
+                {this.renderTableRows()}
+              </tbody>
+            </table>
+          ) : (
+            <div>
+              No users.
+          </div>
+          )
+      }
+      renderTableHeader = () => {
+        return Object.keys(this.state.users[0]).map(attr => <th key={attr}>{attr.toUpperCase()}</th>)
+      }
+      renderTableRows = () => {
+        return this.state.users.map(user => {
+          return (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{`${user.address.street}, ${user.address.city}`}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+              <td>{user.company.name}</td>
+            </tr>
+          )
+        })
+      }
 
-export default UserList;
